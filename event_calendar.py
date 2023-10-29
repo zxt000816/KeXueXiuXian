c176b91a942b4e9f88a2b4db684f475835c92ba2
zyf0706 <zyf13815628123@gmail.com>
2023-10-30 00:01:55 +0900

    modified:   event_calendar.py

--- event_calendar.py
+++ event_calendar.py
@@ -95,12 +95,6 @@ data = data[['活动', '剩余天数', '开始', '结束', '游戏天数(开始)
 # filter out events that are already over
 data = data[data['剩余天数'].apply(lambda x: x.split('天')[0]).astype(int) >= 0]
 
-
-today
-
-datetime.strftime(today, '%Y-%m-%d')
-# print(tabulate(data, headers='keys', tablefmt='psql'))
-
 event_col = f"活动 (当前: {datetime.strftime(today, '%Y-%m-%d')})"
 
 data = data.rename(columns={'活动': event_col})
